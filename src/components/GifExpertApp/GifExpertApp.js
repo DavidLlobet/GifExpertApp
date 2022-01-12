346370dc213b4f03dd5be5d5a2ab987f8f4b7f13
David <david.llobet6@gmail.com>
2022-01-12 11:42:24 +0100

    Add handleAdd function to button

--- src/components/GifExpertApp/GifExpertApp.js
+++ src/components/GifExpertApp/GifExpertApp.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 const GifExpertApp = () => {
+  //const categories = ["One Punch", "Samurai X", "Dragon Ball"];
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+
+  const handleAdd = () => {
+    setCategories([...categories, "Fly"]);
+  };
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+      <button onClick={handleAdd}>Agregar</button>
+      <ul>
+        {categories.map((category, id) => {
+          return <li key={id}>{category}</li>;
+        })}
+      </ul>
     </>
   );
 };
